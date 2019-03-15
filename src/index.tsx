1c711ed3c60b9731c3c2dea89a162f776acdd096
Eetu <eetu.nuorva@gmail.com>
2019-03-15 19:32:32 +0200

    Connect App to store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "mobx-react";
 import "./index.css";
 import App from "./App";
+import AppStore from "./App.store";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider AppStore>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
