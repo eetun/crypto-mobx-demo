2d20f72763c8df9c170e3f4757c7cca7269f3b6d
Eetu <eetu.nuorva@gmail.com>
2019-03-15 19:33:12 +0200

    Install Classnames and connect CurrencySelector to store

--- src/App.store.ts
+++ src/App.store.ts
@@ -3,6 +3,11 @@ import { observable, action } from "mobx";
 export class AppStore {
   @observable currency = "BTC";
   @observable timeInterval = "";
+
+  @action
+  setCurrency = currency => {
+    this.currency = currency;
+  };
 }
 
 export default new AppStore();
--- src/CurrencySelector/CurrencySelector.tsx
+++ src/CurrencySelector/CurrencySelector.tsx
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
+import { observer } from "mobx-react";
+import classnames from "classnames";
 import "./CurrencySelector.css";
+import AppStore from "../App.store";
 
+@observer
 export default class CurrencySelector extends Component {
+  setCurrency = currency => {
+    AppStore.setCurrency(currency);
+  };
+
   render() {
+    const { currency } = AppStore;
+
     return (
       <div className="currencies">
-        <div className="currency content-box active">
+        <div
+          onClick={evt => this.setCurrency("BTC")}
+          className={classnames("currency content-box", {
+            active: currency === "BTC"
+          })}
+        >
           <div className="ticker">BTC</div>
           <div className="price">$ 3500</div>
         </div>
-        <div className="currency content-box">
+        <div
+          onClick={evt => this.setCurrency("ETH")}
+          className={classnames("currency content-box", {
+            active: currency === "ETH"
+          })}
+        >
           <div className="ticker">ETH</div>
           <div className="price">$ 3500</div>
         </div>
